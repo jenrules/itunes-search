0d143667b7ddabb8dee31e8b32b85bbc259c3c2c
JenRules <goldenruel@hotmail.com>
2017-02-01 18:05:56 -0600

    started search

--- itunesdata.js
+++ itunesdata.js
@@ -1,15 +1,17 @@
 $(function() {
-  $("#search").on("click", function() {
-    var searchTerm = $("#searchTerm").val();
-    var url = "https://itunes.apple.com/search?term="+ searchTerm +"&format=json&callback=?";
-    $.ajax({
-      url: url,
-      type:'GET',
-      dataType: 'json',
-      data: function(data, status, jqXHR) {
-        console.log(data)
-      },
-    })
+    $("#search").on("click", function() {
+    	var searchTerm = $("#searchTerm").val();
+		var url = "https://itunes.apple.com/search?limit=10&term="+ searchTerm;
+		$.ajax({
+			url: url,
+			type: 'get',
+			contentType: "jsonp",
+			async: false,
+        	dataType: "jsonp",
+        	success: function(data, status, jqXHL) {
+            console.log(data);
+            }
+          })
       .done(function() {
         console.log("success");
       })
