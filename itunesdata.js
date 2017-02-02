76b7b4dc4e07a878430325c4afe35670dc7b6658
JenRules <goldenruel@hotmail.com>
2017-02-02 10:12:35 -0600

    added design

--- itunesdata.js
+++ itunesdata.js
@@ -1,7 +1,7 @@
 $(function() {
     $("#search").on("click", function() {
     	var searchTerm = $("#searchTerm").val();
-		var url = "https://itunes.apple.com/search?limit=10&term="+ searchTerm;
+		var url = "https://itunes.apple.com/search?limit=5&term="+ searchTerm;
 		$.ajax({
 			url: url,
 			type: 'get',
@@ -11,7 +11,7 @@ $(function() {
         	success: function(songs) {
             $("#output").html();
         		for (var i = 0; i < songs.results.length; i++){
-        			$("#output").prepend("<div><div class='well'><a href=" + songs.results[i] + "><h2>" + songs.results[i].trackName + "</h2>" + " <p>" + songs.results[i].artistName + "</p> </a></div></div>");
+        			$("#output").prepend("<div><div class='well'><a href=" + songs.results[i] + "><h2>" + songs.results[i].trackName + "</h2>" + "<p>" + songs.results[i].artistName + "</p> </a></div></div>");
             }
           }
        })
